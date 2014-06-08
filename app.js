1f93c90dac8c9243aa05630b4e7648a07adaf91d
rakm <rakm@rakm.io>
2014-06-08 12:27:01 -0700

    switch to handlebars

--- app.js
+++ app.js
@@ -1,28 +1,21 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('static-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var routes = require('./routes/index');
-var users = require('./routes/users');
+var express     = require('express');
+var path        = require('path');
+var logger      = require('morgan');
+var expressHbs  = require('express3-handlebars');
 
 var app = express();
 
 // view engine setup
+app.engine('hbs', expressHbs({extname:'hbs', defaultLayout:'main.hbs'}));
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'hbs');
 
-app.use(favicon());
 app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded());
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
+app.use('/', function(req, res) {
+  res.render('index');
+});
 
 /// catch 404 and forward to error handler
 app.use(function(req, res, next) {
